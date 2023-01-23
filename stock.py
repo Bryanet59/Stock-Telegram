3483b2e03072257505dd7cc44b2b03fac417afde
Bryanet59 <123341722+Bryanet59@users.noreply.github.com>
2023-01-23 11:09:29 +0800

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,24 @@
+import os
+import telebot
+import yfinance as yf
+
+
+api_key = "5678018950:AAGcQhCPrLgo7MHcVxuxS1McYgGDu9ddshw"
+bot = telebot.TeleBot(api_key)
+
+@bot.message_handler(commands = ['Greet'])
+def greet(msg):
+    bot.reply_to(msg, "Welcome to Bryanet59 Bot!")
+
+@bot.message_handler(commands=['stock'])
+def get_stock(message):
+    stock_ticker = message.text.split()[1]
+    stock_info = yf.Ticker(stock_ticker).info
+    bot.reply_to(message, f'{stock_ticker} Stock Information: \n'
+                    f'Current Price: {stock_info["regularMarketPrice"]} \n'
+                    f'Previous Close: {stock_info["regularMarketPreviousClose"]} \n'
+                    f'52 Week High: {stock_info["fiftyTwoWeekHigh"]} \n'
+                    f'52 Week Low: {stock_info["fiftyTwoWeekLow"]}')
+
+bot.polling() 
+
